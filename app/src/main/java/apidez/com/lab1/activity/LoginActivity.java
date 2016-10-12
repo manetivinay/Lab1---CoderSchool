b30bae19419d85915b7b9682eae65fc3e890c926
Vinay Maneti <manetivinay@gmail.com>
2016-10-12 20:59:49 +0700

    pass data to Adapter class and finish lab

--- app/src/main/java/apidez/com/lab1/activity/LoginActivity.java
+++ app/src/main/java/apidez/com/lab1/activity/LoginActivity.java
@@ -1,21 +1,54 @@
 package apidez.com.lab1.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.AppCompatTextView;
+import android.view.View;
 import android.widget.Toast;
 
 import apidez.com.lab1.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private AppCompatTextView forgotPassword, signUpFb;
+    private AppCompatEditText mEmail, mPassword;
+    private AppCompatButton mLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        // TODO: Insert your code
+        initView();
     }
 
-    private void showToast(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    private void initView() {
+        forgotPassword = (AppCompatTextView) findViewById(R.id.tvForgotPass);
+        signUpFb = (AppCompatTextView) findViewById(R.id.tvSignUpFb);
+        mEmail = (AppCompatEditText) findViewById(R.id.etEmail);
+        mPassword = (AppCompatEditText) findViewById(R.id.etPassword);
+        mLogin = (AppCompatButton) findViewById(R.id.btnLogin);
+        forgotPassword.setOnClickListener(this);
+        signUpFb.setOnClickListener(this);
+        mLogin.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnLogin:
+                Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(this, StreamActivity.class);
+                startActivity(intent);
+                //startActivity(new Intent(LoginActivity.this, StreamActivity.class));
+                break;
+            case R.id.tvForgotPass:
+                break;
+            case R.id.tvSignUpFb:
+                break;
+        }
     }
 }
--- app/src/main/java/apidez/com/lab1/adapter/PostAdapter.java
+++ app/src/main/java/apidez/com/lab1/adapter/PostAdapter.java
@@ -2,14 +2,18 @@ package apidez.com.lab1.adapter;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.List;
 
+import apidez.com.lab1.R;
 import apidez.com.lab1.model.Post;
 
 /**
@@ -32,18 +36,41 @@ public class PostAdapter extends ArrayAdapter<Post> {
     @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        // TODO: Insert your code
-        // bindViewHolder(position, viewHolder)
-        return convertView;
+        View view;
+        if (convertView == null) {
+            view = LayoutInflater.from(parent.getContext()).
+                    inflate(R.layout.item_post, parent, false);
+        } else {
+            view = convertView;
+        }
+
+        ViewHolder viewHolder = new ViewHolder();
+        viewHolder.tvUsername = (TextView) view.findViewById(R.id.tvUsername);
+        viewHolder.tvDescription = (TextView) view.findViewById(R.id.tvDescription);
+        viewHolder.tvDate = (TextView) view.findViewById(R.id.tvDate);
+        viewHolder.ivAvatar = (ImageView) view.findViewById(R.id.ivAvatar);
+        viewHolder.ivImage = (ImageView) view.findViewById(R.id.ivImage);
+        bindViewHolder(position, viewHolder);
+        view.setTag(viewHolder);
+
+        return view;
     }
 
     private void bindViewHolder(int position, ViewHolder viewHolder) {
         Post post = mPosts.get(position);
-        // TODO: Insert your code
+        viewHolder.tvUsername.setText(post.getUsername());
+        viewHolder.tvDescription.setText(post.getDescription());
+        viewHolder.tvDate.setText(post.getDate());
+        loadImage(viewHolder.ivAvatar, post.getAvatar());
+        loadImage(viewHolder.ivImage, post.getImage());
     }
 
     private void loadImage(ImageView imageView, String path) {
-        // TODO: Insert your code
+        Picasso.with(getContext())
+                .load(path)
+                .placeholder(R.drawable.placeholder)
+                .into(imageView);
+
     }
 
     public class ViewHolder {
